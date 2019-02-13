e371deecdd9fb59d0f61b322cfe05efa681e5851
nimeshngk <nimeshngk@gmail.com>
2019-02-13 17:17:46 +0530

    Added User model

--- myfacebook/Home/views.py
+++ myfacebook/Home/views.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template.context_processors import csrf
+from django.views.decorators.csrf import csrf_protect
 
+@csrf_protect
 def homepage(request):
 	c = {}
 	c.update(csrf(request))
--- myfacebook/Login/models.py
+++ myfacebook/Login/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	user_name = models.CharField(max_length=50)
+	user_id = models.CharField(max_length=20)
+	password = models.CharField(max_length=30)
+	email = models.EmailField(max_length=30)
+
+	def __str__(self):
+		return self.user_name
